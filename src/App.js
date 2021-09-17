992b87e27a11eda6a192d87626748487d664cdfd
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-17 10:25:27 -0400

    finished homepage functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import PizzaForm from "./components/PizzaForm";
+import { useHistory } from "react-router";
 
 const App = () => {
+  const history = useHistory();
+  const goOrder = () => {
+    history.push("/pizza");
+  };
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <Link to="/">Home</Link>
+      </nav>
+      <Route exact path="/">
+        <div className="home-header">
+          <h2>Your Favorite Food, Delivered While Coding</h2>
+          <button onClick={goOrder} id="order-pizza">
+            Order Now
+          </button>
+        </div>
+        );
+      </Route>
+      <Route exact path="/pizza">
+        <PizzaForm />
+      </Route>
+    </div>
   );
 };
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,10 @@ import React from "react";
 import { useHistory } from "react-router";
 
 export default function Home() {
-
-    const history = useHistory();
-    const routeToHome
+  return (
+    <div className="home-header">
+      <h2>Your Favorite Food, Delivered While Coding</h2>
+      <button id="order-pizza">Order Now</button>
+    </div>
+  );
 }
